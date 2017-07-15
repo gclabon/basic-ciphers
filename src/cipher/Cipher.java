0e13ad8c2e190ff878134c8a9797f9b8137e164d
glclabon <glclabon@gmail.com>
2017-07-15 10:50:09 +0100

    Initial commit of ciphers package

---
+++ src/cipher/Cipher.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cipher;
+
+/**
+ * Simple abstract class to implement basic Ciphers (Caesar/Rot13/Rot47)
+ * 
+ * Cipher.java (UTF-8) cipher
+ *
+ * @author Gareth Clabon. 12-Jul-2017 20:37:35
+ */
+public abstract class Cipher
+{
+
+    private static final String LETTERS_26 = "abcdefghijklmnopqrstuvwxyz";
+    private static final String LETTERS_47 = "!\"#$%&'()*+,-./0123456789:"
+            + ";<=>?@ABCDEFGHIJKLMNOPQRST"
+            + "UVWXYZ\\[\\]^_`abcdefghijklm"
+            + "nopqrstuvwxyz{|}~";
+
+    /**
+     * Encodes a String using a simple alphabetical shift
+     *
+     * @param useExtendedCharSet Boolean whether extended character set is
+     * needed for rot47
+     * @param plainText String containing plaintext to be encoded
+     * @param shiftBy int number of Characters to shift by
+     * @return cipherText String containing the encoded plainText
+     */
+    private static String getShiftCipher(String plainText, int shiftBy,
+            boolean useExtendedCharSet)
+    {
+        try
+        {
+            String baseChars;
+            char[] oArray;
+            String cipherText = "";
+
+            //If Rot47 use extended character set, otherwise use lower case
+            if (useExtendedCharSet)
+            {
+                baseChars = LETTERS_47;
+                oArray = plainText.toCharArray();
+            }
+            else
+            {
+                baseChars = LETTERS_26;
+                oArray = plainText.toCharArray();
+            }
+
+            int i;
+            for (char c : oArray)
+            {
+                int cPosition = baseChars.indexOf(c);
+                int lALength = baseChars.length();
+                if ((cPosition + shiftBy) >= lALength)
+                {
+                    i = (cPosition + shiftBy) - lALength;
+                }
+                else
+                {
+                    i = (cPosition + shiftBy);
+                }
+                cipherText += baseChars.charAt(i);
+            }
+
+            return cipherText;
+        }
+        catch (Exception e)
+        {
+            throw new IllegalArgumentException(shiftBy
+                    + "is not a valid shift");
+        }
+    }
+
+    /**
+     * Encodes a String using a simple Caesar Cipher
+     *
+     * @throws IllegalArgumentException If plainText does not contain only
+     * alphabetical chars.
+     * @param plainText String containing plaintext to be encoded
+     * @param shiftBy int Number of Characters to shift by
+     * @return cipherText String containing the encoded plainText
+     */
+    public static String getCaesarCipher(String plainText, int shiftBy)
+            throws IllegalArgumentException
+    {
+        String lowerCasePT = plainText.toLowerCase();
+        //Check plaintext contains only alphabetical chars
+        if (!lowerCasePT.matches("^[" + LETTERS_26 + "]*$"))
+        {
+            throw new IllegalArgumentException(
+                    "Caesar Cipher cannot contain non-alphabetical characters");
+        }
+        else
+        {
+            return getShiftCipher(lowerCasePT, shiftBy, false);
+        }
+    }
+
+    /**
+     * Encodes a String using a simple Rot13 alphabetical shift
+     *
+     * @throws IllegalArgumentException If plainText does not contain only
+     * alphabetical chars.
+     * @param plainText String containing plaintext to be encoded
+     * @return cipherText String containing the encoded plainText
+     */
+    public static String getRot13(String plainText)
+            throws IllegalArgumentException
+    {
+        String lowerCasePT = plainText.toLowerCase();
+        //Check plainText contains only alphabetical chars
+        if (!lowerCasePT.matches("^[" + LETTERS_26 + "]*$"))
+        {
+            throw new IllegalArgumentException(
+                    "Caesar Cipher cannot contain non-alphabetical characters");
+        }
+        else
+        {
+            return getShiftCipher(lowerCasePT, 13, false);
+        }
+    }
+
+    /**
+     * Encodes a String using a simple Rot47 ASCII shift
+     *
+     * @throws IllegalArgumentException If plainText does not contain only
+     * ASCII[33, 126] chars.
+     * @param plainText String containing plaintext to be encoded
+     * @return cipherText String containing the encoded plainText
+     */
+    public static String getRot47(String plainText)
+    {
+        if (!plainText.matches("^[" + LETTERS_47 + "]*$"))
+        {
+            throw new IllegalArgumentException(
+                    "Rot47 must contain only ASCII[33, 126] characters");
+        }
+        else
+        {
+            return getShiftCipher(plainText, 47, true);
+        }
+    }
+
+}
---
+++ test/TestCipher.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import cipher.*;
+
+/**
+ * Basic JUnit tests for cipher class
+ *
+ * @author Gareth Clabon. PI: E445736X
+ */
+public class TestCipher
+{
+
+    public TestCipher()
+    {
+
+    }
+
+    /**
+     * Test shift of 1 at beginning of alphabet
+     */
+    @Test
+    public void test1GetCaesarCipher()
+    {
+        assert Cipher.getCaesarCipher("abc", 1).equals("bcd") : "abc with a shift of 1 should return bcd";
+    }
+
+    /**
+     * Test shift of 1 at end of alphabet
+     */
+    @Test
+    public void test2GetCaesarCipher()
+    {
+        assert Cipher.getCaesarCipher("xyz", 1).equals("yza") : "xyz with a shift of 1 should return yza";
+    }
+
+    /**
+     * Test shift of length of alphabet
+     */
+    @Test
+    public void test3GetCaesarCipher()
+    {
+        assert Cipher.getCaesarCipher("abc", 26).equals("abc") : "abc with a shift of 26 should return abc";
+    }
+
+    /**
+     * Test cannot use a negative shift
+     */
+    @Test
+    public void test4GetCaesarCipher()
+    {
+        boolean thrown = false;
+
+        try
+        {
+            Cipher.getCaesarCipher("abc", -1);
+        }
+        catch (IllegalArgumentException e)
+        {
+            thrown = true;
+        }
+        assert (thrown) : "Illegal Argument Exception should have been thrown for -1 shift";
+    }
+
+    /**
+     * Test cannot use an illegal character
+     */
+    @Test
+    public void test5GetCaesarCipher()
+    {
+        Boolean thrown = false;
+        try
+        {
+            Cipher.getCaesarCipher("!", 1);
+        }
+        catch (IllegalArgumentException e)
+        {
+            thrown = true;
+        }
+        assert (thrown) : "Illegal Argument Exception should have been thrown for !";
+    }
+
+    /**
+     * Test shift at start at alphabet
+     */
+    @Test
+    public void test1GetRot13()
+    {
+        assert Cipher.getRot13("abc").equals("nop") : "abc using Rot13 shoulf return nop";
+    }
+
+    /**
+     * Test shift at end of alphabet
+     */
+    @Test
+    public void test2GetRot13()
+    {
+        assert Cipher.getRot13("xyz").equals("klm") : "xyz using Rot13 should return klm";
+    }
+
+    /**
+     * Test cannot use an illegal character
+     */
+    @Test
+    public void test3GetRot13()
+    {
+        boolean thrown = false;
+
+        try
+        {
+            Cipher.getRot13("!");
+        }
+        catch (IllegalArgumentException e)
+        {
+            thrown = true;
+        }
+        assert (thrown) : "Illegal Argument Exception should have been thrown for !";
+    }
+
+    /**
+     * Test shift at start of ASCII character string
+     */
+    @Test
+    public void test1GetRot47()
+    {
+        assert Cipher.getRot47("abc").equals("234") : "abc using Rot47 should return 234";
+    }
+
+    /**
+     * Test Shift at end of ASCII character string
+     */
+    @Test
+    public void test2GetRot47()
+    {
+        assert Cipher.getRot47("|}~").equals("MNO") : "xyz using Rot47 should return MNO";
+    }
+
+    /**
+     * Test cannot use illegal characters
+     */
+    @Test
+    public void test3GetRot47()
+    {
+        boolean thrown = false;
+
+        try
+        {
+            Cipher.getRot47("é");
+        }
+        catch (IllegalArgumentException e)
+        {
+            thrown = true;
+        }
+        assert (thrown) : "Illegal Argument Exception should have been thrown for é";
+    }
+
+}
